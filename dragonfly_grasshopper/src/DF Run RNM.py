c90cd2b268fcf8bc346370e23604b8bfcbef9df4
Chris Mackey <chris@ladybug.tools>
2022-08-18 11:41:53 -0400

    feat(energy): Add the ability to parse RNM to electrical networks

--- dragonfly_grasshopper/src/DF Run RNM.py
+++ dragonfly_grasshopper/src/DF Run RNM.py
@@ -21,16 +21,27 @@ correctly through RNM.
             in order to run correctly through RNM.
         _scenario: The path to an URBANopt .csv file for the scenario. This CSV
             file can be obtained form the "DF Run URBANopt" component.
+        _ug_ratio_: A number between 0 and 1 for the ratio of overall cables that are
+            underground vs. overhead in the analysis. (Default: 0.9).
+        include_hv_: A boolean to note whether high voltage consumers should be
+            included in the analysis. (Default: False).
+        nodes_per_bldg_: A positive integer for the maximum number of low voltage
+            nodes to represent a single building. (Default: 1).
         _run: Set to "True" to run the geojson and scenario through RNM.
 
     Returns:
         report: Reports, errors, warnings, etc.
-        results: Path to a folder that contains all of the RNM output files.
+        network: The ElectricalNetwork object output from the RNM simulation. The
+            properties of this object can be visualized with the "DF Color
+            Network Attributes" component. However, the network can not be
+            used for OpenDSS simulation (the dss_files below should be used
+            for this purpose).
+        dss_results: Path to the folder containing all of the OpenDSS files.
 """
 
 ghenv.Component.Name = 'DF Run RNM'
 ghenv.Component.NickName = 'RunRNM'
-ghenv.Component.Message = '1.5.0'
+ghenv.Component.Message = '1.5.1'
 ghenv.Component.Category = 'Dragonfly'
 ghenv.Component.SubCategory = '3 :: Energy'
 ghenv.Component.AdditionalHelpFromDocStrings = '0'
@@ -49,16 +60,23 @@ except ImportError as e:
 
 try:  # import the dragonfly_energy dependencies
     from dragonfly_energy.run import run_default_report, run_rnm
+    from dragonfly_energy.opendss.network import ElectricalNetwork
 except ImportError as e:
     raise ImportError('\nFailed to import dragonfly_energy:\n\t{}'.format(e))
 
 try:
     from ladybug_rhino.grasshopper import all_required_inputs
+    from ladybug_rhino.config import units_system
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
 
 
 if all_required_inputs(ghenv.Component) and _run:
+    # set default values
+    ug_ratio = 0.9 if _ug_ratio_ is None else _ug_ratio_
+    lv_only = True if include_hv_ is None else not include_hv_
+    nodes_per_bldg = 1 if nodes_per_bldg_ is None else nodes_per_bldg_
+
     # generate the default scenario report
     def_report = os.path.join(os.path.dirname(_geojson), 'run',
                               'honeybee_scenario', 'default_scenario_report.csv')
@@ -66,4 +84,10 @@ if all_required_inputs(ghenv.Component) and _run:
         run_default_report(_geojson, _scenario)
 
     # execute the simulation with URBANopt CLI
-    results = run_rnm(_geojson, _scenario)
+    results = run_rnm(_geojson, _scenario, ug_ratio, lv_only, nodes_per_bldg)
+
+    # add the kVA to the output GeoJSON file
+    if results is not None:
+        dss_results = os.path.join(results, 'OpenDSS')
+        geo_file = os.path.join(results, 'GeoJSON', 'Distribution_system.json')
+        network = ElectricalNetwork.from_rnm_geojson(geo_file, units=units_system())
