3c90b16274786c4d0544bc2d6c45c764c2aaaca7
Chris Mackey <chris@ladybug.tools>
2025-02-11 14:52:16 -0800

    fix(create): Improve detailed windows with projection_dist

--- dragonfly_grasshopper/src/DF Detailed Windows.py
+++ dragonfly_grasshopper/src/DF Detailed Windows.py
@@ -14,8 +14,14 @@ Add detailed window geometries to Dragonfly Room2Ds.
     Args:
         _df_objs: A Dragonfly Model, Building, Story or Room2D, to which the _windows
             should be added.
-        _windows: A list of Breps that will be added to the input _df_objs as
-            detailed windows.
+        _windows: A list of Breps that will be added to the input _df_objs as detailed
+            windows. This can also be a list of orphaned Honeybee Apertures and/or
+            Doors to be added to the Dragonfly objects. In the case of Doors, they
+            will be assigned to the Dragonfly object as such.
+        project_dist_: An optional number to be used to project the Aperture/Door geometry
+            onto parent Faces. If specified, then sub-faces within this distance
+            of the parent Face will be projected and added. Otherwise,
+            Apertures/Doors will only be added if they are coplanar with a parent Face.
 
     Returns:
         report: Reports, errors, warnings, etc.
@@ -24,7 +30,7 @@ Add detailed window geometries to Dragonfly Room2Ds.
 
 ghenv.Component.Name = "DF Detailed Windows"
 ghenv.Component.NickName = 'DetailedWindows'
-ghenv.Component.Message = '1.8.0'
+ghenv.Component.Message = '1.8.1'
 ghenv.Component.Category = "Dragonfly"
 ghenv.Component.SubCategory = '0 :: Create'
 ghenv.Component.AdditionalHelpFromDocStrings = "5"
@@ -38,6 +44,12 @@ try:  # import the core dragonfly dependencies
 except ImportError as e:
     raise ImportError('\nFailed to import dragonfly:\n\t{}'.format(e))
 
+try:  # import the core honeybee dependencies
+    from honeybee.aperture import Aperture
+    from honeybee.door import Door
+except ImportError as e:
+    raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
+
 try:
     from ladybug_rhino.grasshopper import all_required_inputs
     from ladybug_rhino.config import tolerance, angle_tolerance
@@ -47,9 +59,16 @@ except ImportError as e:
 
 
 if all_required_inputs(ghenv.Component):
-    # duplicate the initial objects and convert windows to Face3D
+    # duplicate the initial objects and convert windows to sub-faces
     df_objs = [obj.duplicate() for obj in _df_objs]
-    win_geo = [f for geo in _windows for f in to_face3d(geo)]
+    win_geo = []
+    for geo in _windows:
+        if isinstance(geo, (Aperture, Door)):
+            win_geo.append(geo)
+        else:
+            for f in to_face3d(geo):
+                win_geo.append(Aperture('Dummy_Ap', f))
+    project_dist = 0 if project_dist_ is None else project_dist_
 
     # collect all of the Room2Ds in the connected dragonfly objects
     room_2ds = []
@@ -65,17 +84,5 @@ if all_required_inputs(ghenv.Component):
 
     # assign the relevant geometries to the Room2Ds
     for room in room_2ds:
-        new_win_pars = []
-        for seg, win_par in zip(room.floor_segments, room.window_parameters):
-            win_to_add = []
-            for geo in win_geo:
-                if DetailedWindows.is_face3d_in_segment_plane(
-                        geo, seg, room.floor_to_ceiling_height,
-                        tolerance, angle_tolerance):
-                    win_to_add.append(geo)
-            if len(win_to_add) != 0:
-                det_win = DetailedWindows.from_face3ds(win_to_add, seg)
-                new_win_pars.append(det_win)
-            else:
-                new_win_pars.append(win_par)
-        room.window_parameters = new_win_pars
+        room.assign_sub_faces(win_geo, project_dist, tolerance=tolerance,
+                              angle_tolerance=angle_tolerance)
